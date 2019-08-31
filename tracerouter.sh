d79a93bc69f5f39c3cb88c28204cfa5fed06037f
Samuel Goldman <6364482+samgoldman@users.noreply.github.com>
2019-08-31 09:04:02 -0400

    Add battery check and cellinfo logging
    Also, change timestamp for trace route logging to use the same value

--- tracerouter.sh
+++ tracerouter.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 
+# Prevent deep-sleep
+termux-wake-lock
+
 while true; do
+	echo --- BATTERY CHECK ---
+	battery="$(termux-battery-status | jq -r '.percentage')"
+	echo TIMESTAMP: $(date +%s) >> battery.status.txt
+	echo $battery >> battery.status.txt
+	if [$battery -lt 30]
+	then
+		termux-volume music 100
+		termxu-tts-speak Warning! Battery Low!
+		termux-volume music 30
+	fi
+	
 	echo --- BEGIN TRACEROUTE SERIES ---
 	while read site; do
 		echo SITE: $site
-		echo TIMESTAMP: $(date +%s) >> $site.txt
-		echo TIMESTAMP: $(date +%s) >> $site.location.txt
+		timestamp="$(date +%s)"
+		echo TIMESTAMP: $timestamp >> $site.txt
+		echo TIMESTAMP: $timestamp >> $site.location.txt
+		echo TIMESTAMP: $timestamp >> $site.cellinfo.txt
 		termux-location >> $site.location.txt &
+		termux-telephony-cellinfo >> $site.cellinfo.txt
 		traceroute $site >> $site.txt &
 		sleep 1
 	done < sites.lst
